a0bb27f6ac8da344a0faaad8018d62761d44014b
MEER ATHIF ALI I <meerathifali@gmail.com>
2021-12-05 08:02:37 +0530

    added heroku build

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,15 @@ app.use(bodyParser.json());
 
 app.use("/users", require("./routes/auth"));
 
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+	// Set Static Folder
+	app.use(express.static("mern-auth/build"));
 
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "mern-auth", "build", "index.html"));
+	});
+}
 
 app.listen(
 	PORT,
